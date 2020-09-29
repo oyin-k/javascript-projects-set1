c9662d92edc7a5aa8515d363234f67c60855d138
oyin-k <oyink7@gmail.com>
2020-09-29 04:56:04 +0100

    Auto text

---
+++ Auto-text/scirpt.js
@@ -0,0 +1,15 @@
+const text = "Everything worthwhile demands dedication and attention to details."
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if(index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
